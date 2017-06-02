eaa9b55bc3d5fb1fddae2b4f7a9ae407a1de8d92
Jens Reimann <jreimann@redhat.com>
2017-06-02 17:05:32 +0200

    Rename test

--- gateway-client-profile-kura/src/test/java/org/eclipse/kapua/gateway/client/kura/PayloadCodecTest.java
+++ gateway-client-profile-kura/src/test/java/org/eclipse/kapua/gateway/client/kura/PayloadCodecTest.java
@@ -23,7 +23,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class PayloadTest {
+public class PayloadCodecTest {
 
     private KuraBinaryPayloadCodec codec;
 
